3784d81a7e8dbb114c54ea29174c3efa8038347f
Jacky Fan <jfan@FQs-MBP.local>
2024-05-10 23:43:54 +1200

    remove the 1st default portfolio

--- src/info/Info.js
+++ src/info/Info.js
@@ -115,15 +115,9 @@ export const info = {
     ],
     portfolio: [ // This is where your portfolio projects will be detailed
         {
-            title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
-            image: mock1
-        },
-        {
             title: "Project 2",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            live: "https://jacky.fan.nz/",
+            source: "https://github.com/RJF-TRUST/rjf-trust.github.io",
             image: mock2
         },
     ]
